fceab8a8357a957950b27a9f42a2263d73c4760f
LucaBoni28 <boninsegna.luca28@gmail.com>
2025-09-25 14:55:44 +0200

    Aggiunta Data Jurassic World

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ const films = [
         duration: "155 min",
         poster: "immagini/jurassic_world_rebirth_ver8.jpg",
         showings: [
-            
+            { date: "2025-07-21", time: "18:30" }
             
         ],
         bookingUrl: "https://ticket.cinebot.it/dro/",
